3b1802ca514ecc5b19a1c6d5b6db5f509fe7b14f
Olivia <oliviahuang@berkeley.edu>
2023-04-22 18:33:52 -0700

    Added my_mingpt and todo's

--- my_mingpt/model.py
+++ my_mingpt/model.py
@@ -24,7 +24,12 @@ class NewGELU(nn.Module):
     Reference: Gaussian Error Linear Units (GELU) paper: https://arxiv.org/abs/1606.08415
     """
     def forward(self, x):
-        return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+        ############################################################################
+        # TODO: implement this function
+        ############################################################################
+        raise NotImplementedError()
+        ############################################################################
+        return output
 
 ### TODO: Update this with some new attention thing
 class MySelfAttention(nn.Module):
