e38236f1c5877fd12642b4fd31a3783390f2c06f
Tracy Xia <tracyxia@berkeley.edu>
2023-05-03 17:57:24 -0700

    add back lines in syn attn

--- my_mingpt/model.py
+++ my_mingpt/model.py
@@ -30,6 +30,29 @@ class MySelfAttention(nn.Module):
         ############################################################################
         # TODO: implement this function
         ############################################################################
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        
+        self.w1 = None
+        self.w2 = None #d_k,T
+        self.b2 = None #T
+        # value projection
+        self.value = None #dmodel,dmodel
+        # output projection
+        self.c_proj = nn.Linear( config.n_embd,  config.n_embd) #dmodel,dmodel
+        # regularization
+        self.attn_drop = nn.Dropout( config.attn_pdrop)
+        self.resid_drop = nn.Dropout( config.resid_pdrop)
+        # causal mask to ensure that attention is only applied to the left in
+        #     the input sequence
+        self.register_buffer("bias", torch.tril(
+            torch.ones( config.block_size,  config.block_size)).view(
+                1, 1,  config.block_size,  config.block_size)) #mask
+        self.n_head =  config.n_head
+        self.block_size =  config.block_size
+
+        # TODO: uniformly initialize self.w2 between -0.001 and 0.001
+        
         raise NotImplementedError()
         ############################################################################
 
@@ -37,6 +60,23 @@ class MySelfAttention(nn.Module):
         ############################################################################
         # TODO: implement this function
         ############################################################################
+        B, T, C = x.size()
+        # @ : The matrix multiplication(s) are done between the last two dimensions
+        d_k = C//self.n_head
+        
+        relu_out = None
+
+        v = None
+        scores = None
+
+        scores = scores[:,:,:T,:T] # to ensure it runs for T<block_size
+        scores = scores.masked_fill(self.bias[:,:,:T,:T]==0,-1e10)
+        prob_attn = F.softmax(scores,dim=-1)
+        y = prob_attn@v
+
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.resid_drop(self.c_proj(y))
+        
         raise NotImplementedError()
         ############################################################################
         return y
